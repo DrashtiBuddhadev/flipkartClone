bbe432f94d8b18e0cd722fdc1859d412cea4023e
DrashtiBuddhadev <buddhadevdrashti@gmail.com>
2024-01-27 15:42:10 +0530

    stash4

--- script.js
+++ script.js
@@ -1,43 +1,29 @@
-let slideIndex = 0;
-showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  
-  // Hide all slides
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  
-  // Move to the next slide
-  slideIndex++;
-  
-  // Reset slideIndex if it exceeds the number of slides
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }    
+let sliderImage=document.querySelector(".slider_image");
+let translate = 0;
 
-  // Display the current slide
-  slides[slideIndex-1].style.display = "block";  
-  
-  // Activate the corresponding dot
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+setInterval(()=>{
+  if(translate>=0 && translate<500){
+      translate += 100.5;
   }
-  dots[slideIndex-1].className += " active";
+  else{
+    translate = 0;
+  }
+  sliderImage.style.transition="1s";
+  sliderImage.style.transform=`translateX(${-translate}%)`
+},4000);
 
-  // Call showSlides function recursively after 2 seconds
-  setTimeout(showSlides, 2000);
-  
-  // Animate sliding effect
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.transition = "transform 1s ease-in-out";
-    slides[i].style.transform = "translateX(-100%)";
+function right(){
+  if(translate<400){
+    translate += 100;
+    sliderImage.style.transition="1s";
+    sliderImage.style.transform=`translateX(${-translate}%)`
   }
-  slides[slideIndex-1].style.transform = "translateX(0)";
 }
-
-
-let 
+function left(){
+  if(translate>0){
+    translate -= 100;
+    sliderImage.style.transition="1s";
+    sliderImage.style.transform=`translateX(${-translate}%)`
+  }
+}
